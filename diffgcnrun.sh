de20abd741001975ea6929fe0b5a6de35f9cb436
guangtao.wang <guangtao.wang@jd.com>
2020-11-18 10:53:41 -0800

    Initial commit

--- diffgcnrun.sh
+++ diffgcnrun.sh
@@ -6,8 +6,10 @@ CODE_PATH=codes
 #The first four parameters must be provided
 #Only used in training
 SEED=$1
+DATANAME=$2
 echo "Start Training......"
 /mnt/cephfs2/asr/users/ming.tu/sgetools/run_gpu.sh python -u $CODE_PATH/example.py \
     --cuda \
     --shuffle \
+    --data $DATANAME\
     --rand_seed $SEED
--- gatrun.sh
+++ gatrun.sh
@@ -6,8 +6,10 @@ CODE_PATH=gatcodes
 #The first four parameters must be provided
 #Only used in training
 SEED=$1
+DATANAME=$2
 echo "Start Training......"
 /mnt/cephfs2/asr/users/ming.tu/sgetools/run_gpu.sh python -u $CODE_PATH/gattrain.py \
     --cuda \
     --shuffle \
+    --dataset $DATANAME\
     --rand_seed $SEED
