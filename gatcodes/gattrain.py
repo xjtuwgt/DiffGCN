a43b735765ffd6d6962c7713158cdaf1ce7cffbc
guangtao.wang <guangtao.wang@jd.com>
2020-11-18 13:16:17 -0800

    Initial commit

--- gatcodes/gattrain.py
+++ gatcodes/gattrain.py
@@ -286,7 +286,7 @@ if __name__ == '__main__':
                         help="indicates whether to use early stop or not")
     parser.add_argument('--fastmode', action="store_true", default=False,
                         help="skip re-evaluate the validation set")
-    parser.add_argument('--rand_seed', default=2,
+    parser.add_argument('--rand_seed', default=3,
                         help="skip re-evaluate the validation set")
     parser.add_argument('--shuffle', default=True, action='store_true',
                         help="random split")
