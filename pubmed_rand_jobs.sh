b3dd12ae14a00c14a3d86756ecd973031c67af65
guangtao.wang <guangtao.wang@jd.com>
2020-11-18 10:32:49 -0800

    Initial commit

--- pubmed_rand_jobs.sh
+++ pubmed_rand_jobs.sh
@@ -6,6 +6,6 @@ for ENTRY in "${JOBS_PATH}"/*.sh; do
   FILE_NAME="$(basename "$ENTRY")"
   echo $FILE_NAME
 #  /mnt/cephfs2/asr/users/ming.tu/software/kaldi/egs/wsj/s5/utils/queue.pl -q g2.q -l gpu=1 $LOGS_PATH/$FILE_NAME.log $ENTRY &
-  /mnt/cephfs2/asr/users/ming.tu/software/kaldi/egs/wsj/s5/utils/queue.pl -q g.q -l gpu=2 $LOGS_PATH/$FILE_NAME.log $ENTRY &
+  /mnt/cephfs2/asr/users/ming.tu/software/kaldi/egs/wsj/s5/utils/queue.pl -q g.q -l gpu=1 $LOGS_PATH/$FILE_NAME.log $ENTRY &
   sleep 3
 done
