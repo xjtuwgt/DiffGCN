cfea597b53cd82c627d6ac238dd28b285deb7b16
guangtao.wang <guangtao.wang@jd.com>
2020-11-17 18:31:24 -0800

    Initial commit

--- codes/example.py
+++ codes/example.py
@@ -49,17 +49,17 @@ class Net(torch.nn.Module):
         super(Net, self).__init__()
         self.conv1 = GCNConv(dataset.num_features, 16, cached=True,
                              normalize=not args.use_gdc)
-        self.layer_norm_1 = torch.nn.LayerNorm(16)
-        self.ff_layer = PositionwiseFeedForward(model_dim=16, d_hidden=8 * 16)
+        # self.layer_norm_1 = torch.nn.LayerNorm(16)
+        # self.ff_layer = PositionwiseFeedForward(model_dim=16, d_hidden=8 * 16)
         self.conv2 = GCNConv(16, dataset.num_classes, cached=True,
                              normalize=not args.use_gdc)
 
     def forward(self):
         x, edge_index, edge_weight = data.x, data.edge_index, data.edge_attr
-        #x = F.relu(self.conv1(x, edge_index, edge_weight))
-        x = self.conv1(x, edge_index, edge_weight)
-        norm_x = self.layer_norm_1(x)
-        x = self.ff_layer(x + norm_x)
+        x = F.relu(self.conv1(x, edge_index, edge_weight))
+        # x = self.conv1(x, edge_index, edge_weight)
+        # norm_x = self.layer_norm_1(x)
+        # x = self.ff_layer(x + norm_x)
         x = F.dropout(x, training=self.training)
         x = self.conv2(x, edge_index, edge_weight)
         return F.log_softmax(x, dim=1)
