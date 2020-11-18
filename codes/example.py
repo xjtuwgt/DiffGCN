f010e9ff2d30452525cd38a41261d8cc4b5ef5f8
guangtao.wang <guangtao.wang@jd.com>
2020-11-17 18:22:03 -0800

    Initial commit

--- codes/example.py
+++ codes/example.py
@@ -69,7 +69,7 @@ def test():
 
 
 best_val_acc = test_acc = 0
-for epoch in range(1, 201):
+for epoch in range(1, 401):
     train()
     train_acc, val_acc, tmp_test_acc = test()
     if val_acc > best_val_acc:
