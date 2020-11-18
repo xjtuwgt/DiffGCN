7d447671139370bd62c5f4eaf198a4724ea85e90
guangtao.wang <guangtao.wang@jd.com>
2020-11-18 11:35:55 -0800

    Initial commit

--- cora_rand_jobs.sh
+++ cora_rand_jobs.sh
@@ -5,8 +5,8 @@ for ENTRY in "${JOBS_PATH}"/*.sh; do
   chmod +x $ENTRY
   FILE_NAME="$(basename "$ENTRY")"
   echo $FILE_NAME
-#  /mnt/cephfs2/asr/users/ming.tu/software/kaldi/egs/wsj/s5/utils/queue.pl -q g2.q -l gpu=1 $LOGS_PATH/$FILE_NAME.log $ENTRY &
+  /mnt/cephfs2/asr/users/ming.tu/software/kaldi/egs/wsj/s5/utils/queue.pl -q g2.q -l gpu=1 $LOGS_PATH/$FILE_NAME.log $ENTRY &
 #  sleep 3
-  /mnt/cephfs2/asr/users/ming.tu/software/kaldi/egs/wsj/s5/utils/queue.pl -q g.q -l gpu=1 $LOGS_PATH/$FILE_NAME.log $ENTRY &
+#  /mnt/cephfs2/asr/users/ming.tu/software/kaldi/egs/wsj/s5/utils/queue.pl -q g.q -l gpu=1 $LOGS_PATH/$FILE_NAME.log $ENTRY &
   sleep 3
 done
