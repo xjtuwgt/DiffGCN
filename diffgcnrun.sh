22dabb123531065a57925989d8cb43b82850bbc9
guangtao.wang <guangtao.wang@jd.com>
2020-11-18 10:47:38 -0800

    Initial commit

--- diffgcnrun.sh
+++ diffgcnrun.sh
@@ -2,12 +2,12 @@
 
 python -u -c 'import torch; print(torch.__version__)'
 
-CODE_PATH=gatcodes
+CODE_PATH=codes
 #The first four parameters must be provided
 #Only used in training
 SEED=$1
 echo "Start Training......"
-/mnt/cephfs2/asr/users/ming.tu/sgetools/run_gpu.sh python -u $CODE_PATH/gattrain.py \
+/mnt/cephfs2/asr/users/ming.tu/sgetools/run_gpu.sh python -u $CODE_PATH/example.py \
     --cuda \
     --shuffle \
     --rand_seed $SEED
