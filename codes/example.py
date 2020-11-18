f8da507a4e7ec2f1777a36ef95aff866b6b4154a
guangtao.wang <guangtao.wang@jd.com>
2020-11-18 12:24:29 -0800

    Initial commit

--- codes/example.py
+++ codes/example.py
@@ -126,11 +126,8 @@ def main(args):
                 res_x = self.res_fc(x)
             else:
                 res_x = x
-            norm_x = self.layer_norm_1(convx)
+            norm_x = F.dropout(self.layer_norm_1(convx), training=self.training)
             norm_x = norm_x + res_x
-
-            norm_x = F.dropout(norm_x, training=self.training)
-
             x_ff = self.ff_layer(norm_x)
             x_ff = F.dropout(x_ff, training=self.training)
             x = norm_x + x_ff
