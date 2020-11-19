d6fee7d5f745d067c1016d3b774147e6a4362c33
guangtao.wang <guangtao.wang@jd.com>
2020-11-19 12:28:56 -0800

    Initial commit

--- gatcodes/gat.py
+++ gatcodes/gat.py
@@ -89,7 +89,7 @@ class GATFF(nn.Module):
         # input projection (no residual)
         self.gat_layers.append(GATConvFF(
             in_dim, num_hidden, heads[0],
-            feat_drop, attn_drop, negative_slope, False, self.activation))
+            feat_drop, attn_drop, negative_slope, True, self.activation))
         # hidden layers
         for l in range(1, num_layers):
             # due to multi-head, the in_dim = num_hidden * num_heads
