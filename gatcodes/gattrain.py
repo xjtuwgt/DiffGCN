c29e28f503d8f93ec5836454fbc45ec4922b47d4
guangtao.wang <guangtao.wang@jd.com>
2020-11-19 12:23:30 -0800

    Initial commit

--- gatcodes/gattrain.py
+++ gatcodes/gattrain.py
@@ -227,7 +227,7 @@ def main(args):
 
 def model_selection(args):
     num_hidden_range = [8, 16, 32, 64]
-    in_drop_range = [0.4, 0.6]
+    in_drop_range = [0.2, 0.4, 0.6]
     att_drop_range = [0.4, 0.6]
     lr_range = [0.005]
     best_acc = 0
